03362986a55980fba659a77414b781ac991c7440
ilanribeiro <ilanribeiro@gmail.com>
2021-03-16 21:56:20 -0300

    subindo arquivo das aulas

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+function botao(element) {
+  // const grats = document.getElementById('grats')
+  element.innerHTML = "Redirecionando";
+  // window.open('https://linkedin.com');
+  // window.location.href = 'https://udemy.com';
+}
+
+
+// onmouseover
+// onmouseout
+// onload
+// 
+
+
+// let nome = prompt("Qual o seu nome?")
+// console.log(`Bem vindo ${nome}`)
+
+// let frase = "Japão é o melhor time do mundo"
+// console.log(frase)
+// console.log(frase.replace("Japão", "Brasil"))
+
+
+// let date = new Date()
+// console.log(date)
+// console.log(date.getMonth()+1)
+// console.log(date.getFullYear())
